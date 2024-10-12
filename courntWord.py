ecbc873923ebfb0bc6bf07cc8405c6310bb7fe8b
Guedalia Sebbah <gsebbah@g.jct.ac.il>
2024-10-12 21:08:17 +0300

    good

--- courntWord.py
+++ courntWord.py
@@ -19,8 +19,8 @@ def count_words_and_letters(text):
 
 # Add the new columns
 for df in [df1, df2,df3,df4]:
-    df['word'] = df['Text'].apply(lambda x: count_words_and_letters(x)[0] if pd.notnull(x) else 0)
-    df['character'] = df['Text'].apply(lambda x: count_words_and_letters(x)[1] if pd.notnull(x) else 0)
+    df['num of character'] = df['Text'].apply(lambda x: count_words_and_letters(x)[1] if pd.notnull(x) else 0)
+    df['num of word'] = df['Text'].apply(lambda x: count_words_and_letters(x)[0] if pd.notnull(x) else 0)
 
 # Save the updated Excel files
 df1.to_excel('SuspectedCounted.xlsx', index=False)
